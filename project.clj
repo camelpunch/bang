699aa095991fa5e078918d0d4faec7a0a12531cd
Andrew Bruce <andrew@camelpunch.com>
2014-06-05 20:34:02 -0700

    make uberjar-specific config

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject bang "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :main bang.console
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [jline "1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
+  :profiles {:uberjar {:aot :all
+                       :main bang.console}
+             :dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}})
