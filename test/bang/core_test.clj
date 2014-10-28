536765fada2dc73ddf8dfbf076cb3fdbf3a6b87f
Andrew Bruce <andrew@camelpunch.com>
2014-10-28 00:06:12 +0000

    Remove confusing stuff from test

--- test/bang/core_test.clj
+++ test/bang/core_test.clj
@@ -2,22 +2,20 @@
   (:require [clojure.test :refer :all]
             [bang.core :refer :all]))
 
-(def some-game-state identity)
-
 (deftest player-movement
   (testing "player moves right when input is R"
-    (is (= (some-game-state "  |")
+    (is (= "  |"
            (next-game-state " | "
                             "R"))))
   (testing "player moves left when input is L"
-    (is (= (some-game-state "|  ")
+    (is (= "|  "
            (next-game-state " | "
                             "L"))))
   (testing "player movement wraps on left side"
-    (is (= (some-game-state "      |")
+    (is (= "      |"
            (next-game-state "|      "
                             "L"))))
   (testing "player movement wraps on right side"
-    (is (= (some-game-state "|      ")
+    (is (= "|      "
            (next-game-state "      |"
                             "R")))))
