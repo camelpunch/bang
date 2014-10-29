51bc9d408c2c4e2940d5597497d6aaedca8c5ef5
Andrew Bruce <andrew@camelpunch.com>
2014-10-29 22:55:20 +0000

    Crude full-screen console board

--- src/bang/console.clj
+++ src/bang/console.clj
@@ -1,22 +1,30 @@
 (ns bang.console
   (:import jline.Terminal)
   (:gen-class)
-  (:require [bang.core :refer [next-game-state]]))
+  (:require [bang.core :refer [next-game-state]]
+            [clojure.string :refer [join replace-first]]))
 
-(def game (atom "              |              "))
+(def game (atom nil))
 
 (defn char-to-input [char]
   (cond
-   (= char 68) "L"
+   (= char 65) "U"
+   (= char 66) "D"
    (= char 67) "R"
+   (= char 68) "L"
    :else nil))
 
 (defn show-game [game-state]
-  (print "\r" game-state "\r")
+  (print (join "\n" game-state))
   (flush))
 
 (defn -main [& args]
-  (let [term (Terminal/getTerminal)]
+  (let [term (Terminal/getTerminal)
+        empty-board (repeat (.getTerminalHeight term)
+                            (apply str (repeat (.getTerminalWidth term) \space)))
+        line-with-player (replace-first (first empty-board) #"." "|")
+        board-with-player (concat [line-with-player] (rest empty-board))]
+    (reset! game board-with-player)
     (show-game @game)
     (while true
       (let [char (.readCharacter term System/in)]
